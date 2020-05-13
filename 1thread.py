9037b12ed151ffa19cc1f355c25e5b451b4daa41
Manish Kumar <40482920+ghostmk@users.noreply.github.com>
2020-05-13 20:43:50 +0530

    Update 1thread.py

--- 1thread.py
+++ 1thread.py
@@ -11,7 +11,7 @@ place.replace(" ", "+")
 radius=str(input('Enter the Radius:'))
 
 
-response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+place+'&key=AIzaSyByoWKvAT9C_5uwRQJfUlwFF1jkO_kbOoc')
+response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+place+'&key=enter_apikey_here')
 resp_json_payload = response.json()
 lat1 = str(resp_json_payload['results'][0]['geometry']['location']['lat'])
 long1 = str(resp_json_payload['results'][0]['geometry']['location']['lng'])
@@ -20,7 +20,7 @@ long1 = str(resp_json_payload['results'][0]['geometry']['location']['lng'])
 # place=str(input('Enter Second Location:'))
 # place.replace(" ", "+")
 
-# response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+place+'&key=AIzaSyByoWKvAT9C_5uwRQJfUlwFF1jkO_kbOoc')
+# response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+place+'&key=enter_apikey_here')
 # resp_json_payload = response.json()
 # lat2 = str(resp_json_payload['results'][0]['geometry']['location']['lat'])
 # long2 = str(resp_json_payload['results'][0]['geometry']['location']['lng'])
@@ -30,7 +30,7 @@ long1 = str(resp_json_payload['results'][0]['geometry']['location']['lng'])
 
 
 
-url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+lat1+','+long1+'&radius='+radius+'&type=hospital&key=AIzaSyByoWKvAT9C_5uwRQJfUlwFF1jkO_kbOoc'
+url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+lat1+','+long1+'&radius='+radius+'&type=hospital&key=enter_apikey_here'
 
 headers={'content-type':'application/json',
          'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:63.0) Gecko/20100101 Firefox/63.0'}
